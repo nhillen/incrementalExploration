447ac3621a76ffd3da3d12ae4e9774c17fa60bdb
nhillen <nhillen@gmail.com>
2015-11-23 17:30:06 -0800

    Changed format of things to get everything working

--- js/game.js
+++ js/game.js
@@ -1,141 +1,160 @@
 var widgetBar = $("#widgetCounter");
 var checkVals = ["totalWidgets", "factories", "factoryCost", "widgetsPerSecond"];
 
+function drawObject() {
+    if (!this.uiObject) {
+        this.uiObject = $(this.UID);
+    }
+    
+    if (counters[this.costType] >= this.cost) {
+        this.uiObject.show();
+        this.uiObject.prop('disabled', false);
+    } else {
+        this.uiObject.prop('disabled', true);
+    }
+    
+    this.uiObject.prop('value', this.buttonTag + ": " + this.cost);
+}
+
+function findObject(someObject){
+    var returnObject;
+    //Get the ID of the passed in element
+    var checkUID = "#" + someObject.id;
+    _.each(allButtons, function (name) {
+        if(name.UID === checkUID){
+            returnObject = name;
+        }
+    });
+    
+    return returnObject;
+}
+
+function purchase() {
+    var controlObject = findObject(this);
+    
+    //If we have enough resources to buy the thing
+    if (counters[controlObject.costType] >= controlObject.cost)
+    {
+        //Remove the amount
+        counters[controlObject.costType] -= controlObject.cost;
+
+        //Set the new price
+        controlObject.cost = Math.round(controlObject.cost * controlObject.costScaler);
+
+        //Set the new amount
+        counters[controlObject.gainType] += controlObject.gainAmount;
+    }  
+    
+    drawUI();
+}
+
 
 var counters = {
-	"none" : 0,
-	"time" : 0,
-	"ore" : 0,
-	"money" : 0,
-	"widgets" : 0,
-	"widgetsPerClick" : 1,
-	"widgetsPerSecond" : 0,
-	"tools" : 0
+    "none": 0,
+    "time": 0,
+    "ore": 0,
+    "money": 0,
+    "widgets": 0,
+    "widgetsPerClick": 1,
+    "widgetsPerSecond": 0,
+    "tools": 0
 }
 
 // this instantiates the Producer Class
-var makeWidgetButton = new Producer({
-	"UIname"     : "#btnMakeWidget",
-	"showReq"    : 0,
-	"showType"   : "none",
-	"cost"       : 0,
-	"costType"	 : "none",
-	"costScaler" : 0,
-	"buttonTag"  : "Make Widget",
-	"gainType"	 : "widgets",
-	"gainAmount" : 1
-});
+var makeWidgetButton = {
+    UID: "#btnMakeWidget",
+    showReq: 0,
+    showType: "none",
+    cost: 0,
+    costType: "none",
+    costScaler: 0,
+    buttonTag: "Make Widget",
+    gainType: "widgets",
+    gainAmount: 1,
+    draw: drawObject,
+    purchase: purchase
+};
 
-var sellWidgetButton = new Producer({
-	"UIname"     : "#btnSellWidgets",
-	"showReq"    : 1,
-	"showType"   : "widgets",
-	"cost"       : 1,
-	"costType"	 : "widgets",
-	"costScaler" : 1,
-	"buttonTag"  : "Sell Widgets",
-	"gainType"	 : "money",
-	"gainAmount" : 5
-});
+var sellWidgetButton = {
+    UID: "#btnSellWidget",
+    showReq: 1,
+    showType: "widgets",
+    cost: 1,
+    costType: "widgets",
+    costScaler: 1,
+    buttonTag: "Sell Widgets",
+    gainType: "money",
+    gainAmount: 5,
+    draw: drawObject,
+    purchase: purchase
+};
 
-var toolsButton = new Producer({
-	"UIname"     : "#btnBuyTool",
-	"showReq"    : 1,
-	"showType"   : "widgets",
-	"cost"       : 50,
-	"costType"	 : "money",
-	"costScaler" : 1.25,
-	"buttonTag"  : "Tools Cost",
-	"gainType"	 : "tools",
-	"gainAmount" : 1
-});
+var toolsButton = {
+    UID: "#btnBuyTool",
+    showReq: 1,
+    showType: "widgets",
+    cost: 50,
+    costType: "money",
+    costScaler: 1.25,
+    buttonTag: "Tools Cost",
+    gainType: "tools",
+    gainAmount: 1,
+    draw: drawObject,
+    purchase: purchase
+};
 
-function Producer( vars )
-{
-	var bar;
-	var v = vars;/* VARIABLES ***
-	"UIname"     = "button",
-	"showReq"    = 0,
-	"showType"   = "money",
-	"cost"       = 0,
-	"costType"	 = "money",
-	"costScaler" = 1.2,
-	"buttonTag"  = "cost",
-	"gainType"	 = "tools",
-	"gainAmount" = 1
-    *****************************/ 
-	
-	this.draw = function(){
-		// If the button hasn't been initialized, then find it
-			if( ! bar )
-				bar = $(this.v["UIname"]);
-			
-		if( counters[this.v["showReqType"]] >= this.v["showReq"] ){
-			this.bar.show();
-			this.bar.prop('disabled', false);
-        
-		} else {
-			this.bar.prop('disabled', true);
-		}
-		bar.prop( 'value', v["buttonTag"]+": "+v["cost"] );
-	}
-	
-	this.clicked = function(){
-		if( counters[v[costType]] >= v[cost] )
-		{
-			counters[v[costType]] -= v[cost];
-			Math.round( v[cost] = v[cost]*v[costScaler] ) ;
-			counters[v[gainType]] += v[gainAmount];
-		}
-		drawUI();		
-	}
-}
+var allButtons = [makeWidgetButton, sellWidgetButton, toolsButton];
+
 
 /*** MAIN LOOP ****************************************/
 
-$(document).ready(function(){
+$(document).ready(function () {
     readSaveData();
+
+    _.each(allButtons, function (button) {
+        $(button.UID).click(button.purchase);
+    });
 });
 
-window.setInterval(function(){
+window.setInterval(function () {
     runLoop();
     drawUI();
 }, 1000);
 
-function runLoop(){
+function runLoop() {
     totalWidgets += widgetsPerSecond;
-	counters["time"]++;
+    counters["time"]++;
 }
 
-function drawUI(){
+function drawUI() {
     var outputText =
-		counters["time"] + " seconds" + "<br/>"+
-		"$"+counters["money"] + "<br/>" +
-        counters["widgets"] + " total widgets" + "<br/>" +
-        counters["widgetsPerSecond"] + " widgets per second" + "<br/>" +
-        counters["tools"] + " tools" + "<br/>";
-	widgetBar.html(outputText);
-		
-	// Draw all of the buttons	
-		makeWidgetButton.draw();	
-		sellWidgetButton.draw();
-		toolsButton.draw();
+            counters["time"] + " seconds" + "<br/>" +
+            "$" + counters["money"] + "<br/>" +
+            counters["widgets"] + " total widgets" + "<br/>" +
+            counters["widgetsPerSecond"] + " widgets per second" + "<br/>" +
+            counters["tools"] + " tools" + "<br/>";
+    widgetBar.html(outputText);
+
+    // Draw all of the buttons	
+    _.each(allButtons, function (button) {
+        button.draw();
+    });
 }
 
 /*** SAVE IO *******************************************/
 
-function readSaveData(){
-    _.each(checkVals,function(name){
-       var temp = window.localStorage.getItem(name);
-       if(temp){
-           window[name] = Number(temp);
-       }
+function readSaveData() {
+    _.each(checkVals, function (name) {
+        var temp = window.localStorage.getItem(name);
+        if (temp) {
+            window[name] = Number(temp);
+        }
     });
 }
 
-function writeData(){
-    _.each(checkVals,function(name){
-       window.localStorage.setItem(name, window[name]);
+function writeData() {
+    _.each(checkVals, function (name) {
+        window.localStorage.setItem(name, window[name]);
     });
 }
 
